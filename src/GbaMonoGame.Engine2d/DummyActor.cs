5fa6360fb13f578ea436179a9737cea671227c1d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-02-02 20:17:56 +0100

    Improve DummyActor to determine animation and bg prio

--- src/GbaMonoGame.Engine2d/DummyActor.cs
+++ src/GbaMonoGame.Engine2d/DummyActor.cs
@@ -1,4 +1,6 @@
-﻿namespace GbaMonoGame.Engine2d;
+﻿using GbaMonoGame.TgxEngine;
+
+namespace GbaMonoGame.Engine2d;
 
 // Temporary class
 public class DummyActor : BaseActor
@@ -6,5 +8,13 @@ public class DummyActor : BaseActor
     public DummyActor(int instanceId, Scene2D scene, ActorResource actorResource) : base(instanceId, scene, actorResource)
     {
         Logger.NotImplemented("Not implemented actor of type {0}", actorResource.Type);
+
+        if (actorResource.FirstActionId < actorResource.Model.Actions.Length)
+            AnimatedObject.CurrentAnimation = actorResource.Model.Actions[actorResource.FirstActionId].AnimationIndex;
+        else
+            AnimatedObject.CurrentAnimation = actorResource.FirstActionId;
+
+        if (Scene.Playfield is TgxPlayfieldMode7)        
+            AnimatedObject.BgPriority = 0;
     }
 }
