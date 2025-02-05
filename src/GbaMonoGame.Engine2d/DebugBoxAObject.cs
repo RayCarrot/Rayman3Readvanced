798dcee8e0eee60206f074174c5386114c2c8e43
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-02-05 14:08:30 +0100

    Improve drawing debug boxes and points

--- src/GbaMonoGame.Engine2d/DebugBoxAObject.cs
+++ src/GbaMonoGame.Engine2d/DebugBoxAObject.cs
@@ -11,22 +11,18 @@ public class DebugBoxAObject : AObject
 {
     public Vector2 Size { get; set; }
     public Color Color { get; set; }
-    public float Thickness { get; set; } = 1;
     public bool IsFilled { get; set; }
 
-    private void DrawLine(Vector2 point1, Vector2 point2)
+    private void DrawBox(Vector2 position, Vector2 size, float alpha = 1)
     {
-        float distance = Vector2.Distance(point1, point2);
-        float angle = MathF.Atan2(point2.Y - point1.Y, point2.X - point1.X);
-
         Gfx.AddSprite(new Sprite
         {
             Texture = Gfx.Pixel,
-            Position = (point1 + point2) * 0.5f,
+            Position = position,
             Priority = BgPriority,
-            Center = true,
-            AffineMatrix = new AffineMatrix(angle, new Vector2(distance, Thickness)),
-            Color = Color,
+            Center = false,
+            AffineMatrix = new AffineMatrix(0, size),
+            Color = new Color(Color, alpha),
             Shader = Shader,
             RenderContext = RenderContext,
         });
@@ -34,28 +30,19 @@ public class DebugBoxAObject : AObject
 
     public override void Execute(Action<short> soundEventCallback)
     {
+        if (Size == Vector2.Zero)
+            return;
+
         if (IsFilled)
         {
-            const float alpha = 0.5f;
-
-            Gfx.AddSprite(new Sprite
-            {
-                Texture = Gfx.Pixel,
-                Position = ScreenPos,
-                Priority = BgPriority,
-                Center = false,
-                AffineMatrix = new AffineMatrix(0, Size),
-                Color = new Color(Color, alpha),
-                Shader = Shader,
-                RenderContext = RenderContext,
-            });
+            DrawBox(ScreenPos, Size, 0.5f);
         }
         else
         {
-            DrawLine(ScreenPos, ScreenPos + new Vector2(Size.X, 0)); // Top
-            DrawLine(ScreenPos, ScreenPos + new Vector2(0, Size.Y)); // Left
-            DrawLine(ScreenPos + new Vector2(0, Size.Y), ScreenPos + Size); // Bottom
-            DrawLine(ScreenPos + new Vector2(Size.X, 0), ScreenPos + Size); // Right
+            DrawBox(ScreenPos, new Vector2(Size.X, 1)); // Top
+            DrawBox(ScreenPos, new Vector2(1, Size.Y)); // Left
+            DrawBox(ScreenPos + Size, -new Vector2(1, Size.Y)); // Bottom
+            DrawBox(ScreenPos + Size, -new Vector2(Size.X, 1)); // Right
         }
     }
 }
--- src/GbaMonoGame.Engine2d/DebugPointAObject.cs
+++ src/GbaMonoGame.Engine2d/DebugPointAObject.cs
@@ -9,23 +9,19 @@ namespace GbaMonoGame.Engine2d;
 /// </summary>
 public class DebugPointAObject : AObject
 {
-    public float Size { get; set; } = 10;
-    public float Thickness { get; set; } = 1;
+    public float Size { get; set; } = 5;
     public Color Color { get; set; }
 
-    private void DrawLine(Vector2 point1, Vector2 point2)
+    private void DrawBox(Vector2 position, Vector2 size, float alpha = 1)
     {
-        float distance = Vector2.Distance(point1, point2);
-        float angle = MathF.Atan2(point2.Y - point1.Y, point2.X - point1.X);
-
         Gfx.AddSprite(new Sprite
         {
             Texture = Gfx.Pixel,
-            Position = (point1 + point2) * 0.5f,
+            Position = position,
             Priority = BgPriority,
-            Center = true,
-            AffineMatrix = new AffineMatrix(angle, new Vector2(distance, Thickness)),
-            Color = Color,
+            Center = false,
+            AffineMatrix = new AffineMatrix(0, size),
+            Color = new Color(Color, alpha),
             Shader = Shader,
             RenderContext = RenderContext,
         });
@@ -33,7 +29,9 @@ public class DebugPointAObject : AObject
 
     public override void Execute(Action<short> soundEventCallback)
     {
-        DrawLine(ScreenPos - new Vector2(Size / 2, 0), ScreenPos + new Vector2(Size / 2, 0));
-        DrawLine(ScreenPos - new Vector2(0, Size / 2), ScreenPos + new Vector2(0, Size / 2));
+        DrawBox(ScreenPos - new Vector2(0, Size), new Vector2(1, Size)); // Top
+        DrawBox(ScreenPos - new Vector2(Size, 0), new Vector2(Size, 1)); // Left
+        DrawBox(ScreenPos + new Vector2(0, 1), new Vector2(1, Size)); // Bottom
+        DrawBox(ScreenPos + new Vector2(1, 0), new Vector2(Size, 1)); // Right
     }
 }
