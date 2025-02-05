fa1c9588db2ba52e50799f69048654ebc531b7f9
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-02-05 19:39:03 +0100

    Show projected Mode7 debug position point

--- src/GbaMonoGame.Engine2d/DebugBoxColor.cs
+++ src/GbaMonoGame.Engine2d/DebugBoxColor.cs
@@ -6,6 +6,7 @@ internal static class DebugBoxColor
 {
     // Colors source: https://materialui.co/colors
     public static Color PositionPoint { get; } = new(233, 30, 99); // Pink 500
+    public static Color PositionPointProjected { get; } = new(96, 125, 139); // Blue Grey 500
     public static Color CaptorBox { get; } = new(255, 152, 0); // Orange 500
     public static Color DetectionBox { get; } = new(255, 193, 7); // Amber 500
     public static Color ActionBox { get; } = new(63, 81, 181); // Indigo 500
--- src/GbaMonoGame.Engine2d/Mode7Actor.cs
+++ src/GbaMonoGame.Engine2d/Mode7Actor.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using GbaMonoGame.AnimEngine;
+using GbaMonoGame.TgxEngine;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
 
 namespace GbaMonoGame.Engine2d;
 
@@ -15,7 +18,15 @@ public abstract class Mode7Actor : MovableActor
         ZPos = 0;
         RenderHeight = 32;
         AnimatedObject.BgPriority = 0;
+
+        _debugPositionPointProjectedAObject = new DebugPointAObject()
+        {
+            Color = DebugBoxColor.PositionPointProjected,
+            RenderContext = Scene.RenderContext,
+        };
     }
+
+    private readonly DebugPointAObject _debugPositionPointProjectedAObject;
 
     public short ZPos { get; set; }
     public bool IsAffine { get; set; }
@@ -36,5 +47,23 @@ public abstract class Mode7Actor : MovableActor
 
         if (newActionId != ActionId)
             ActionId = newActionId;
+    }
+
+    public override void DrawDebugBoxes(AnimationPlayer animationPlayer)
+    {
+        base.DrawDebugBoxes(animationPlayer);
+
+        TgxCameraMode7 cam = (TgxCameraMode7)Scene.Playfield.Camera;
+
+        // Get the 3D position
+        Vector3 actorPos = new(Position, 0);
+
+        // Project to the screen
+        Vector3 screenPos = cam.Project(actorPos);
+
+        // Set the screen position
+        _debugPositionPointProjectedAObject.ScreenPos = new Vector2(screenPos.X, screenPos.Y);
+
+        animationPlayer.PlayFront(_debugPositionPointProjectedAObject);
     }
 }
