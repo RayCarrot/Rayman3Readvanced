a60461d198a702aa8629dd8ad6d89a7ee4b683d8
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-02-06 18:51:55 +0100

    Implement pausing in Mode7 levels

--- src/GbaMonoGame.Rayman3/Game/Level/FrameMode7.cs
+++ src/GbaMonoGame.Rayman3/Game/Level/FrameMode7.cs
@@ -1,4 +1,5 @@
-﻿using BinarySerializer.Ubisoft.GbaEngine.Rayman3;
+﻿using BinarySerializer.Ubisoft.GbaEngine;
+using BinarySerializer.Ubisoft.GbaEngine.Rayman3;
 using GbaMonoGame.Engine2d;
 using GbaMonoGame.TgxEngine;
 using Action = System.Action;
@@ -12,7 +13,7 @@ public class FrameMode7 : Frame, IHasScene, IHasPlayfield
     public FrameMode7(MapId mapId)
     {
         GameInfo.SetNextMapId(mapId);
-        MultiplayerPauseFrame = 0;
+        PausedMachineId = 0;
     }
 
     #endregion
@@ -22,12 +23,14 @@ public class FrameMode7 : Frame, IHasScene, IHasPlayfield
     public Scene2D Scene { get; set; }
     public Action CurrentStepAction { get; set; }
 
+    public FadeControl SavedFadeControl { get; set; }
+
     public TransitionsFX TransitionsFX { get; set; }
     public Dialog UserInfo { get; set; }
     public PauseDialog PauseDialog { get; set; }
 
     public bool CanPause { get; set; }
-    public byte MultiplayerPauseFrame { get; set; }
+    public int PausedMachineId { get; set; }
     public uint Timer { get; set; }
 
     #endregion
@@ -114,7 +117,98 @@ public class FrameMode7 : Frame, IHasScene, IHasPlayfield
         Scene.AnimationPlayer.Execute();
         LevelMusicManager.Step();
 
-        // TODO: Handle pausing
+        if (!RSMultiplayer.IsActive)
+        {
+            if (JoyPad.IsButtonJustPressed(GbaInput.Start) && CanPause)
+            {
+                CurrentStepAction = Step_Pause_Init;
+                GameTime.Pause();
+            }
+        }
+        else
+        {
+            // TODO: Implement
+        }
+    }
+
+    public void Step_Pause_Init()
+    {
+        SavedFadeControl = Gfx.FadeControl;
+
+        // Fade after drawing screen 0, thus only leaving the sprites 0 as not faded
+        Gfx.FadeControl = new FadeControl(FadeMode.BrightnessDecrease, FadeFlags.Screen0);
+        Gfx.GbaFade = 6;
+
+        UserInfo.ProcessMessage(this, Message.UserInfo_Pause);
+
+        SoundEventsManager.FinishReplacingAllSongs();
+        SoundEventsManager.PauseAllSongs();
+
+        Scene.ProcessDialogs();
+        Scene.Playfield.Step();
+        Scene.AnimationPlayer.Execute();
+        CurrentStepAction = Step_Pause_AddDialog;
+    }
+
+    public void Step_Pause_AddDialog()
+    {
+        PauseDialog.PausedMachineId = PausedMachineId;
+
+        Scene.AddDialog(PauseDialog, true, false);
+
+        Scene.Step();
+        UserInfo.Draw(Scene.AnimationPlayer);
+        Scene.Playfield.Step();
+        Scene.AnimationPlayer.Execute();
+        CurrentStepAction = Step_Pause_Paused;
+    }
+
+    public void Step_Pause_Paused()
+    {
+        if (PauseDialog.DrawStep == PauseDialog.PauseDialogDrawStep.Hide)
+            CurrentStepAction = Step_Pause_UnInit;
+
+        Scene.Step();
+
+        // The original game doesn't have this check, but since we're still running the game loop
+        // while in the simulated sleep mode we have to make sure to not draw the HUD then
+        if (!PauseDialog.IsInSleepMode)
+            UserInfo.Draw(Scene.AnimationPlayer);
+
+        Scene.Playfield.Step();
+        Scene.AnimationPlayer.Execute();
+    }
+
+    public void Step_Pause_UnInit()
+    {
+        Scene.RemoveLastDialog();
+
+        Scene.RefreshDialogs();
+        Scene.ProcessDialogs();
+
+        // We probably don't need to do this, but in the original game it needs to reload things like
+        // palette indexes since it might be allocated differently in VRAM after unpausing.
+        foreach (GameObject gameObj in Scene.KnotManager.GameObjects)
+            gameObj.ProcessMessage(this, Message.ReloadAnimation);
+
+        Scene.Playfield.Step();
+        Scene.AnimationPlayer.Execute();
+        CurrentStepAction = Step_Pause_Resume;
+    }
+
+    public void Step_Pause_Resume()
+    {
+        Gfx.FadeControl = SavedFadeControl;
+        Gfx.Fade = 0;
+
+        UserInfo.ProcessMessage(this, Message.UserInfo_Unpause);
+
+        SoundEventsManager.ResumeAllSongs();
+        Scene.Step();
+        Scene.Playfield.Step();
+        Scene.AnimationPlayer.Execute();
+        CurrentStepAction = Step_Normal;
+        GameTime.Resume();
     }
 
     #endregion
