c5b477d674a4d634d8ac330c48f21205365b792e
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-02-22 21:18:15 +0100

    Implement SamMode7, not done tether sparkles yet

--- src/GbaMonoGame.Rayman3/Game/Actor/Mode7/SamMode7.Fsm.cs
+++ src/GbaMonoGame.Rayman3/Game/Actor/Mode7/SamMode7.Fsm.cs
@@ -1,6 +1,165 @@
-﻿namespace GbaMonoGame.Rayman3;
+﻿using System;
+using BinarySerializer.Ubisoft.GbaEngine;
+using GbaMonoGame.Engine2d;
+
+namespace GbaMonoGame.Rayman3;
 
 public partial class SamMode7
 {
+    public bool Fsm_Init(FsmAction action)
+    {
+        switch (action)
+        {
+            case FsmAction.Init:
+                // Do nothing
+                break;
 
+            case FsmAction.Step:
+                WaterSplashObj = Scene.CreateProjectile<WaterSplashMode7>(ActorType.WaterSplashMode7);
+                if (WaterSplashObj != null)
+                {
+                    WaterSplashObj.ActionId = WaterSplashMode7.Action.SurfWaves;
+                    WaterSplashObj.Position = Position;
+                    WaterSplashObj.ChangeAction();
+                }
+
+                State.MoveTo(Fsm_Move);
+                return false;
+
+            case FsmAction.UnInit:
+                ShouldDraw = true;
+                break;
+        }
+
+        return true;
+    }
+
+    public bool Fsm_Move(FsmAction action)
+    {
+        switch (action)
+        {
+            case FsmAction.Init:
+                Timer = 0;
+                TargetDirection = Direction;
+                MechModel.Speed = MechModel.Speed with { X = 2.25f };
+                break;
+
+            case FsmAction.Step:
+                SetMode7DirectionalAction();
+
+                // Huh?
+                Vector2 pos = Position + Direction.ToDirectionalVector() * new Vector2(MathHelpers.FromFixedPoint(0x12FD00)) * new Vector2(1, -1);
+                PhysicalType type = Scene.GetPhysicalType(pos);
+
+                TargetDirection = type.Value switch
+                {
+                    PhysicalTypeValue.MovingPlatform_Left => 0x80,
+                    PhysicalTypeValue.MovingPlatform_Right => 0,
+                    PhysicalTypeValue.MovingPlatform_Up => 0x40,
+                    PhysicalTypeValue.MovingPlatform_Down => 0xC0,
+                    PhysicalTypeValue.MovingPlatform_DownLeft => 0xA0,
+                    PhysicalTypeValue.MovingPlatform_DownRight => 0xE0,
+                    PhysicalTypeValue.MovingPlatform_UpRight => 0x20,
+                    PhysicalTypeValue.MovingPlatform_UpLeft => 0x60,
+                    _ => TargetDirection
+                };
+
+                // NOTE: The game has no tolerance check, but it doesn't use floats so it doesn't need to
+                if (Math.Abs(Direction - TargetDirection) >= 1)
+                {
+                    if (Direction - TargetDirection < Angle256.Half)
+                        Direction -= 2;
+                    else
+                        Direction += 2;
+                }
+                else
+                {
+                    Direction = TargetDirection;
+                }
+
+                Timer++;
+
+                if (WaterSplashObj != null)
+                    WaterSplashObj.Position = Position + Speed * -4;
+
+                if (type == PhysicalTypeValue.MovingPlatform_Stop)
+                {
+                    State.MoveTo(Fsm_End);
+                    return false;
+                }
+
+                // TODO: Only enable in debug mode - this is a leftover prototype cheat
+                if (JoyPad.IsButtonJustPressed(GbaInput.Select) && JoyPad.IsButtonPressed(GbaInput.L))
+                {
+                    State.MoveTo(Fsm_End);
+                    return false;
+                }
+                break;
+
+            case FsmAction.UnInit:
+                // Do nothing
+                break;
+        }
+
+        return true;
+    }
+
+    public bool Fsm_End(FsmAction action)
+    {
+        switch (action)
+        {
+            case FsmAction.Init:
+                Scene.Camera.ProcessMessage(this, Message.CamMode7_Spin, true);
+                Scene.MainActor.ProcessMessage(this, Message.Main_LevelEnd);
+                Timer = 0;
+                break;
+
+            case FsmAction.Step:
+                SetMode7DirectionalAction();
+
+                // Slow down
+                if (MechModel.Speed.X > 0)
+                    MechModel.Speed -= new Vector2(MathHelpers.FromFixedPoint(0x800), 0);
+
+                Timer++;
+
+                FrameMode7 frame = (FrameMode7)Frame.Current;
+
+                // Fade out
+                if (Timer == 140)
+                {
+                    frame.TransitionsFX.FadeOutInit(2 / 16f);
+                    frame.CanPause = false;
+                }
+                // Stop music
+                else if (Timer == 216)
+                {
+                    SoundEventsManager.StopAllSongs();
+                }
+                // Save and end level
+                else if (Timer == 218)
+                {
+                    if (GameInfo.PersistentInfo.LastCompletedLevel < (int)GameInfo.MapId &&
+                        GameInfo.MapId < MapId.Bonus1 &&
+                        GameInfo.PersistentInfo.LastCompletedLevel < (int)GameInfo.MapId)
+                    {
+                        GameInfo.PersistentInfo.LastCompletedLevel = (byte)GameInfo.MapId;
+                    }
+
+                    frame.EndOfFrame = true;
+                    GameInfo.PersistentInfo.LastPlayedLevel = (byte)GameInfo.MapId;
+                    GameInfo.Save(GameInfo.CurrentSlot);
+                }
+
+                if (WaterSplashObj != null)
+                    WaterSplashObj.Position = Position;
+                break;
+
+            case FsmAction.UnInit:
+                // Do nothing
+                break;
+        }
+
+        return true;
+    }
 }
--- src/GbaMonoGame.Rayman3/Game/Actor/Mode7/SamMode7.cs
+++ src/GbaMonoGame.Rayman3/Game/Actor/Mode7/SamMode7.cs
@@ -1,4 +1,5 @@
-﻿using GbaMonoGame.Engine2d;
+﻿using GbaMonoGame.AnimEngine;
+using GbaMonoGame.Engine2d;
 
 namespace GbaMonoGame.Rayman3;
 
@@ -6,6 +7,62 @@ public sealed partial class SamMode7 : Mode7Actor
 {
     public SamMode7(int instanceId, Scene2D scene, ActorResource actorResource) : base(instanceId, scene, actorResource)
     {
+        ShouldDraw = false;
+        AnimatedObject.BgPriority = 0;
+        RenderHeight = 16;
+        Direction = Angle256.Quarter * 3; // 3/4
+        field_0x70 = 1;
+        WaterSplashObj = null;
+        TetherItems = null;
 
+        State.SetTo(Fsm_Init);
+    }
+
+    // TODO: Name and implement
+    public byte field_0x70 { get; set; }
+    public object TetherItems { get; set; }
+
+    public byte Timer { get; set; }
+    public Angle256 TargetDirection { get; set; }
+    public WaterSplashMode7 WaterSplashObj { get; set; }
+    public bool ShouldDraw { get; set; }
+
+    private void SetMode7DirectionalAction()
+    {
+        SetMode7DirectionalAction(0, 5);
+    }
+
+    protected override bool ProcessMessageImpl(object sender, Message message, object param)
+    {
+        if (base.ProcessMessageImpl(sender, message, param))
+            return false;
+
+        switch (message)
+        {
+            case Message.Main_Damaged2:
+                field_0x70 = 0;
+                return true;
+
+            case Message.ReloadAnimation:
+                // Don't need to do anything. The original game reloads the tether animations here.
+                return false;
+
+            default:
+                return false;
+        }
+    }
+
+    public override void Init(ActorResource actorResource)
+    {
+        // TODO: Create tether items
+    }
+
+    public override void Draw(AnimationPlayer animationPlayer, bool forceDraw)
+    {
+        if (!ShouldDraw)
+            return;
+
+        // TODO: Implement drawing tether items
+        base.Draw(animationPlayer, forceDraw);
     }
 }
