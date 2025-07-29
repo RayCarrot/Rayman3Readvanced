21c1d40db3282c13d836b06ae609086b8f854b63
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-07-29 21:43:09 +0200

    Fix N-Gage pirates being enabled before spawning

--- src/GbaMonoGame.Rayman3/Game/Level/World_1/NGageAscension.cs
+++ src/GbaMonoGame.Rayman3/Game/Level/World_1/NGageAscension.cs
@@ -1,4 +1,5 @@
 ﻿using BinarySerializer.Ubisoft.GbaEngine;
+using GbaMonoGame.Engine2d;
 using GbaMonoGame.TgxEngine;
 
 namespace GbaMonoGame.Rayman3;
@@ -16,6 +17,16 @@ public class NGageAscension : FrameSideScroller
             TgxTileLayer cloudsLayer = ((TgxPlayfield2D)Scene.Playfield).TileLayers[0];
             TextureScreenRenderer renderer = (TextureScreenRenderer)cloudsLayer.Screen.Renderer;
             cloudsLayer.Screen.Renderer = new LevelCloudsRenderer(renderer.Texture, [32, 120, 227]);
+        }
+
+        // The red pirate enemies are enabled by default even though they're linked to a captor. This
+        // is not normally visible in the original game due to the small screen width, but here it
+        // becomes very noticeable in high resolution.
+        if (Engine.ActiveConfig.Tweaks.FixBugs)
+        {
+            Scene.GetGameObject(77).ProcessMessage(this, Message.Destroy);
+            Scene.GetGameObject(80).ProcessMessage(this, Message.Destroy);
+            Scene.GetGameObject(82).ProcessMessage(this, Message.Destroy);
         }
     }
 }
