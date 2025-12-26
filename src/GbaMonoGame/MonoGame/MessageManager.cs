22beb527b7cf6d1ec2920c40b640035d59316113
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-12-26 17:00:07 +0100

    Force windowed when displaying WinForms message box

--- src/GbaMonoGame/MonoGame/MessageManager.cs
+++ src/GbaMonoGame/MonoGame/MessageManager.cs
@@ -20,9 +20,18 @@ public class MessageManager
 
         if (_messageQueue.TryDequeue(out Message msg))
         {
-            // Use WinForms messagebox on Windows since it looks nicer than the custom MonoGame one
 #if WINDOWSDX
+            // Force windowed mode for this
+            DisplayMode prevDisplayMode = Engine.GameWindow.DisplayMode;
+            if (Engine.GameWindow.DisplayMode != DisplayMode.Windowed)
+                Engine.GameWindow.DisplayMode = DisplayMode.Windowed;
+
+            // Use WinForms messagebox on Windows since it looks nicer than the custom MonoGame one
             System.Windows.Forms.MessageBox.Show(msg.Text, msg.Header, System.Windows.Forms.MessageBoxButtons.OK);
+            
+            // Restore the display mode
+            if (prevDisplayMode != DisplayMode.Windowed)
+                Engine.GameWindow.DisplayMode = prevDisplayMode;
 #else
             _messageTask = Microsoft.Xna.Framework.Input.MessageBox.Show(msg.Header, msg.Text, ["OK"]);
 #endif
