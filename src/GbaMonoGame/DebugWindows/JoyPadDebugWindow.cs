0fbcbf39b4af2c958bcb9f2e164ca63183fd913f
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-12-29 20:06:24 +0100

    Add debug option to load BizHawk TAS

--- src/GbaMonoGame/DebugWindows/JoyPadDebugWindow.cs
+++ src/GbaMonoGame/DebugWindows/JoyPadDebugWindow.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using BinarySerializer.Ubisoft.GbaEngine;
@@ -11,6 +13,58 @@ public class JoyPadDebugWindow : DebugWindow
 {
     public override string Name => "JoyPad";
 
+    public void LoadBizHawkTas(string filePath)
+    {
+        using FileStream fileStream = File.OpenRead(filePath);
+        using ZipArchive bk2 = new(fileStream, ZipArchiveMode.Read);
+
+        ZipArchiveEntry entry = bk2.GetEntry("Input Log.txt");
+
+        if (entry == null)
+            return;
+
+        using Stream logFileStream = entry.Open();
+        using StreamReader reader = new(logFileStream);
+
+        List<GbaInput> inputs = new();
+        const string prefix = "|    0,    0,    0,    0,";
+        GbaInput[] inputSeq =
+        [
+            GbaInput.Up,
+            GbaInput.Down,
+            GbaInput.Left,
+            GbaInput.Right,
+            GbaInput.Start,
+            GbaInput.Select,
+            GbaInput.B,
+            GbaInput.A,
+            GbaInput.L,
+            GbaInput.R
+        ];
+
+        while (reader.ReadLine() is { } line)
+        {
+            GbaInput input = GbaInput.Valid;
+
+            if (line.StartsWith(prefix))
+            {
+                line = line[prefix.Length..];
+
+                for (int i = 0; i < inputSeq.Length; i++)
+                {
+                    if (line[i] != '.')
+                        input |= inputSeq[i];
+                }
+            }
+
+            inputs.Add(input);
+        }
+
+        inputs.Add(GbaInput.None);
+
+        JoyPad.SetReplayData(inputs.ToArray());
+    }
+
     public override void Draw(DebugLayout debugLayout, DebugLayoutTextureManager textureManager)
     {
         ImGui.SeparatorText("General");
@@ -18,6 +72,17 @@ public class JoyPadDebugWindow : DebugWindow
         ImGui.Text($"KeyStatus: {JoyPad.Current.KeyStatus}");
         ImGui.Text($"KeyTriggers: {JoyPad.Current.KeyTriggers}");
         ImGui.Text($"Replay: {JoyPad.Current.IsInReplayMode}");
+
+        if (ImGui.Button("Load BizHawk TAS (.bk2)"))
+        {
+            string filePath = FileDialog.OpenFile("Select the TAS file", new FileDialog.FileFilter("bk2", "BK2 files"));
+            if (filePath != null)
+                LoadBizHawkTas(filePath);
+        }
+
+        ImGui.SameLine();
+        if (ImGui.Button("End replay"))
+            JoyPad.Current.ReplayData = null;
 
         ImGui.Spacing();
         ImGui.SeparatorText("Record");
