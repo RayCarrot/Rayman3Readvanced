0f4d231fa7465fca33e65e07518c8a628510212d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-03-19 21:35:43 +0100

    Optionally fix Scaleman shadow layer

--- src/GbaMonoGame.Rayman3/Game/Actor/SideScroller/Enemies/ScalemanShadow.cs
+++ src/GbaMonoGame.Rayman3/Game/Actor/SideScroller/Enemies/ScalemanShadow.cs
@@ -7,6 +7,10 @@ public sealed class ScalemanShadow : BaseActor
     public ScalemanShadow(int instanceId, Scene2D scene, ActorResource actorResource) 
         : base(instanceId, scene, actorResource)
     {
+        // Optionally make the shadow render behind the boss to avoid it overlapping on top of it
+        if (Engine.ActiveConfig.Tweaks.VisualImprovements)
+            AnimatedObject.ObjPriority = 33;
+        
         State.SetTo(null);
     }
 }
