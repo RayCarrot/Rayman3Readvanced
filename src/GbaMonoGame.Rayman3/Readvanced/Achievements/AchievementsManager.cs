30c96eadca37beafb39deded339f0ba288812b32
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-04-29 21:37:21 +0200

    Define remaining progression-based achievements

--- src/GbaMonoGame.Rayman3/Readvanced/Achievements/AchievementsManager.cs
+++ src/GbaMonoGame.Rayman3/Readvanced/Achievements/AchievementsManager.cs
@@ -11,86 +11,135 @@ public static class AchievementsManager
     {
         Achievements = new Dictionary<AchievementId, AchievementInfo>()
         {
-            // TODO: Implement all achievements and trigger them
             [AchievementId.CompleteWorld1] = new(
-                id: AchievementId.CompleteWorld1,
+                id: AchievementId.CompleteWorld1, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
-                title: "Unknown name",
+                title: "Unknown name", // TODO: Name
                 description: "Complete Forgotten Forests"),
             [AchievementId.CompleteWorld2] = new(
-                id: AchievementId.CompleteWorld2,
+                id: AchievementId.CompleteWorld2, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "A Bad Night's Sleep",
                 description: "Complete Haunted Dreams"),
             [AchievementId.CompleteWorld3] = new(
-                id: AchievementId.CompleteWorld3,
+                id: AchievementId.CompleteWorld3, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "It's Getting Hot in Here",
                 description: "Complete Magmacosm"),
             [AchievementId.CompleteWorld4] = new(
-                id: AchievementId.CompleteWorld4,
+                id: AchievementId.CompleteWorld4, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "Storming the Stronghold",
                 description: "Complete Pirate Stronghold"),
             [AchievementId.DefeatBossMachine] = new(
-                id: AchievementId.DefeatBossMachine,
+                id: AchievementId.DefeatBossMachine, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "Horrible Machine",
                 description: "Defeat the machine"),
             [AchievementId.DefeatJano] = new(
-                id: AchievementId.DefeatJano,
+                id: AchievementId.DefeatJano, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "Skull Bash",
                 description: "Defeat Jano"),
             [AchievementId.DefeatRocky] = new(
-                id: AchievementId.DefeatRocky,
+                id: AchievementId.DefeatRocky, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "And a Hard Place",
                 description: "Defeat Rocky"),
             [AchievementId.DefeatScaleman] = new(
-                id: AchievementId.DefeatScaleman,
+                id: AchievementId.DefeatScaleman, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "Try that for Size!",
                 description: "Defeat Scaleman"),
             [AchievementId.DefeatGrolgoth] = new(
-                id: AchievementId.DefeatGrolgoth,
+                id: AchievementId.DefeatGrolgoth, // TODO: Trigger
                 isGold: false,
-                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
                 bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
                 title: "The Grand Finale",
                 description: "Defeat Razorbeard"),
+            [AchievementId.CollectAllWorld1] = new(
+                id: AchievementId.CollectAllWorld1, // TODO: Trigger
+                isGold: true,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
+                bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
+                title: "Forget Anyone?",
+                description: "Collect all lums and cages in Forgotten Forests"),
+            [AchievementId.CollectAllWorld2] = new(
+                id: AchievementId.CollectAllWorld2, // TODO: Trigger
+                isGold: true,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
+                bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
+                title: "Lucid Dreams",
+                description: "Collect all lums and cages in Haunted Dreams"),
+            [AchievementId.CollectAllWorld3] = new(
+                id: AchievementId.CollectAllWorld3, // TODO: Trigger
+                isGold: true,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
+                bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
+                title: "Through Fire and Flames",
+                description: "Collect all lums and cages in Magmacosm"),
+            [AchievementId.CollectAllWorld4] = new(
+                id: AchievementId.CollectAllWorld4, // TODO: Trigger
+                isGold: true,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
+                bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
+                title: "Unknown name", // TODO: Name
+                description: "Collect all lums and cages in Pirate Stronghold"),
+            [AchievementId.Collect1000thLum] = new(
+                id: AchievementId.Collect1000thLum, // TODO: Trigger
+                isGold: true,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
+                bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
+                title: "How Did It Get Here?",
+                description: "Collect the secret lum"),
+            [AchievementId.CompleteGCNBonus] = new(
+                id: AchievementId.CompleteGCNBonus, // TODO: Trigger
+                isGold: true,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
+                bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
+                title: "Square Eyes",
+                description: "Complete all GameCube bonus levels",
+                exclusivePlatform: Platform.GBA),
+            [AchievementId.CollectAllLives] = new(
+                id: AchievementId.CollectAllLives, // TODO: Trigger
+                isGold: true,
+                smallIconTexturePath: Assets.AchievementIcon32px_LockedTexture, // TODO: Texture
+                bigIconTexturePath: Assets.AchievementIcon64px_LockedTexture,
+                title: "Hide and Seek Master",
+                description: "Find all hidden lives"),
             [AchievementId.TimeAttackBronze] = new(
-                id: AchievementId.TimeAttackBronze,
+                id: AchievementId.TimeAttackBronze, // TODO: Trigger
                 isGold: false,
                 smallIconTexturePath: Assets.AchievementIcon32px_TimeAttackBronzeTexture,
                 bigIconTexturePath: Assets.AchievementIcon64px_TimeAttackBronzeTexture,
-                title: "Unknown name",
+                title: "Unknown name", // TODO: Name
                 description: "Earn bronze in every time attack level"),
             [AchievementId.TimeAttackSilver] = new(
-                id: AchievementId.TimeAttackSilver,
+                id: AchievementId.TimeAttackSilver, // TODO: Trigger
                 isGold: false,
                 smallIconTexturePath: Assets.AchievementIcon32px_TimeAttackSilverTexture,
                 bigIconTexturePath: Assets.AchievementIcon64px_TimeAttackSilverTexture,
-                title: "Unknown name",
+                title: "Unknown name", // TODO: Name
                 description: "Earn silver in every time attack level"),
             [AchievementId.TimeAttackGold] = new(
-                id: AchievementId.TimeAttackGold,
+                id: AchievementId.TimeAttackGold, // TODO: Trigger
                 isGold: true,
                 smallIconTexturePath: Assets.AchievementIcon32px_TimeAttackGoldTexture,
                 bigIconTexturePath: Assets.AchievementIcon64px_TimeAttackGoldTexture,
