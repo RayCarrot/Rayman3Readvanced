9628cab860e25462b06e633213c0ed6bf693747d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-05-24 12:21:36 +0200

    Update benchmarks

--- src/BinarySerializer.GbaEngine.Benchmarks/Program.cs
+++ src/BinarySerializer.GbaEngine.Benchmarks/Program.cs
@@ -7,21 +7,32 @@ using BinarySerializer.Ubisoft.GbaEngine.Rayman3;
 
 BenchmarkRunner.Run<LoadRom>();
 
-// | Method                               | Mean            | Error         | StdDev        | Gen0        | Gen1        | Gen2       | Allocated     |
-// |------------------------------------- |----------------:|--------------:|--------------:|------------:|------------:|-----------:|--------------:|
-// | ReadFirstScene                       |    49,895.46 us |    486.787 us |    431.524 us |   5700.0000 |   2300.0000 |   900.0000 |   44103.97 KB |
-// | ReadFirstScene_HighPerformance       |    12,583.37 us |    194.951 us |    182.358 us |   1828.1250 |    812.5000 |   265.6250 |   13510.47 KB |
-// | ReadAllScenes                        | 2,424,941.17 us | 13,130.517 us | 11,639.861 us | 329000.0000 | 139000.0000 | 49000.0000 | 2538589.55 KB |
-// | ReadAllScenes_HighPerformance        |   691,361.42 us | 13,772.627 us | 16,395.334 us | 131000.0000 |  55000.0000 | 18000.0000 |  972235.45 KB |
-// | ReadAllScenes_Cached                 |   433,602.06 us |  8,520.259 us | 11,374.301 us | 105000.0000 |  31000.0000 |  2000.0000 |  891443.05 KB |
-// | ReadAllScenes_Cached_HighPerformance |   379,691.14 us |  7,242.914 us |  8,340.950 us |  99000.0000 |  29000.0000 |  3000.0000 |  827843.16 KB |
-// | ReadSoundBank                        |       322.96 us |      2.062 us |      1.828 us |    112.3047 |     49.8047 |          - |     918.36 KB |
-// | ReadFont                             |        13.87 us |      0.138 us |      0.122 us |      2.4414 |           - |          - |      19.97 KB |
-// | ReadTextBanks                        |     3,556.04 us |     40.489 us |     33.811 us |    906.2500 |    800.7813 |          - |    7422.67 KB |
-// | ReadLevelInfo                        |        37.83 us |      0.753 us |      2.048 us |      7.3242 |      0.4272 |          - |      60.13 KB |
-// | ReadStoryActs                        |     7,722.41 us |    133.813 us |    173.995 us |    546.8750 |    234.3750 |    78.1250 |    4212.66 KB |
-// | ReadBitmaps                          |       686.91 us |      3.889 us |      3.447 us |     55.6641 |     26.3672 |          - |     460.91 KB |
-// | ReadNewPowerReplayData               |       210.00 us |      2.270 us |      2.124 us |     27.5879 |      0.7324 |          - |     226.63 KB |
+// Original:
+// | Method                 | Mean            | Error         | StdDev        | Gen0        | Gen1        | Gen2       | Allocated     |
+// |------------------------|----------------:|--------------:|--------------:|------------:|------------:|-----------:|--------------:|
+// | ReadFirstScene         |    49,895.46 us |    486.787 us |    431.524 us |   5700.0000 |   2300.0000 |   900.0000 |   44103.97 KB |
+// | ReadAllScenes          | 2,424,941.17 us | 13,130.517 us | 11,639.861 us | 329000.0000 | 139000.0000 | 49000.0000 | 2538589.55 KB |
+// | ReadSoundBank          |       322.96 us |      2.062 us |      1.828 us |    112.3047 |     49.8047 |          - |     918.36 KB |
+// | ReadFont               |        13.87 us |      0.138 us |      0.122 us |      2.4414 |           - |          - |      19.97 KB |
+// | ReadTextBanks          |     3,556.04 us |     40.489 us |     33.811 us |    906.2500 |    800.7813 |          - |    7422.67 KB |
+// | ReadLevelInfo          |        37.83 us |      0.753 us |      2.048 us |      7.3242 |      0.4272 |          - |      60.13 KB |
+// | ReadStoryActs          |     7,722.41 us |    133.813 us |    173.995 us |    546.8750 |    234.3750 |    78.1250 |    4212.66 KB |
+// | ReadBitmaps            |       686.91 us |      3.889 us |      3.447 us |     55.6641 |     26.3672 |          - |     460.91 KB |
+// | ReadNewPowerReplayData |       210.00 us |      2.270 us |      2.124 us |     27.5879 |      0.7324 |          - |     226.63 KB |
+//
+// Current:
+// | Method                 | Mean          | Error        | StdDev        | Gen0        | Gen1       | Gen2       | Allocated    |
+// |----------------------- |--------------:|-------------:|--------------:|------------:|-----------:|-----------:|-------------:|
+// | ReadFirstScene         |   8,926.94 us |    54.956 us |     48.717 us |   1515.6250 |   359.3750 |    78.1250 |  12101.65 KB |
+// | ReadAllScenes          | 588,062.60 us | 7,112.026 us |  6,304.626 us | 115000.0000 | 29000.0000 |  8000.0000 |  915640.8 KB |
+// | ReadSoundBank          |     365.64 us |     7.280 us |      6.079 us |    112.3047 |    49.8047 |          - |    918.36 KB |
+// | ReadFont               |      14.86 us |     0.221 us |      0.207 us |      2.4414 |          - |          - |     19.97 KB |
+// | ReadTextBanks          |   3,567.46 us |    59.786 us |     49.924 us |    906.2500 |   800.7813 |          - |   7422.67 KB |
+// | ReadLevelInfo          |      34.22 us |     0.545 us |      0.425 us |      7.3242 |     0.4272 |          - |     60.13 KB |
+// | ReadStoryActs          |   7,936.85 us |    34.127 us |     30.252 us |    546.8750 |   250.0000 |    78.1250 |   4211.95 KB |
+// | ReadBitmaps            |     715.39 us |    14.018 us |     22.234 us |     55.6641 |    26.3672 |          - |    460.81 KB |
+// | ReadNewPowerReplayData |     126.45 us |     2.413 us |      2.872 us |     19.2871 |     0.4883 |          - |    159.49 KB |
+
 
 [MemoryDiagnoser]
 public class LoadRom
@@ -86,16 +97,6 @@ public class LoadRom
                 loader.ReadScene(i);
                 loader.Context.Cache.Clear();
             }
-        });
-    }
-
-    [Benchmark]
-    public void ReadAllScenes_Cached()
-    {
-        Read(static loader =>
-        {
-            for (int i = 0; i < 65; i++)
-                loader.ReadScene(i);
         });
     }
 
