887dda2071c9a12d693270da95f2c897ff5bef81
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-06-09 20:12:04 +0200

    Update unit tests for latest changes

--- src/GbaMonoGame.Rayman3.Tests/Mocks/MockApplicationManager.cs
+++ src/GbaMonoGame.Rayman3.Tests/Mocks/MockApplicationManager.cs
@@ -3,6 +3,7 @@ namespace GbaMonoGame.Rayman3.Tests;
 public class MockApplicationManager : IApplicationManager
 {
     public bool IsActive => true;
+    public float Framerate { get; set; }
     
     public void BeginLoad() { }
     public void Exit() { }
